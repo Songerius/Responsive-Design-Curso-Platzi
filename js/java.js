bcdc26e481d494c1a50730e19a64efd3bd86981e
Songer <ramosger_14@hotmail.com>
2020-09-14 20:34:45 -0600

    version final del sitio web responsive con el curso de platzi

---
+++ js/java.js
@@ -0,0 +1,33 @@
+// console.log("hola mundo");
+
+const ipad = window.matchMedia('screen and (max-width: 767px)');
+
+// console.log(document.querySelector('.menu'));
+
+const menu = document.querySelector('.menu');
+const burguerButton = document.querySelector('#burger-menu');
+
+
+ipad.addListener(validation)
+
+function validation(event) {
+    if (event.matches) {
+        burguerButton.addEventListener('click', hideShow);
+    } else {
+        burguerButton.removeEventListener('click', hideShow);
+    }
+}
+validation(ipad);
+
+function hideShow(){
+    if (menu.classList.contains('is-active')) {
+        menu.classList.remove('is-active');
+    } 
+    else
+    {
+        menu.classList.add('is-active');
+
+    }
+
+};
+
